b65f3356c9127c10fa66cbba14776165788e6596
大貫　弘貴 <ohnukihiroki@oonukihiroshitakanoMacBook-Pro.local>
2021-10-05 22:39:23 +0900

    first

---
+++ ft_isalnum.c
@@ -0,0 +1,15 @@
+int	ft_isalnum(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 'A' && str[i] <= 'Z') && !(str[i] >= 'a' && str[i] <= 'z') && !(str[i] >= '0' && str[i] <= '9'))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
---
+++ ft_isalpha.c
@@ -0,0 +1,15 @@
+int	ft_isalpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 'A' && str[i] <= 'Z') && !(str[i] >= 'a' && str[i] <= 'z'))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
