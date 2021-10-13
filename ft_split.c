9f4a8f511c8dd9a5b097edae50881e2d73a87576
大貫　弘貴 <ohnukihiroki@oonukihiroshitakanoMacBook-Pro.local>
2021-10-13 21:07:03 +0900

    ft_split.c comp

--- ft_split.c
+++ ft_split.c
@@ -3,61 +3,91 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hohnuki <hohnuki@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ohnukihiroki <ohnukihiroki@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 15:46:13 by hohnuki           #+#    #+#             */
-/*   Updated: 2021/10/12 20:31:14 by hohnuki          ###   ########.fr       */
+/*   Updated: 2021/10/13 21:04:22 by ohnukihirok      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-char	*ft_word_cut(char *s, int start, int i)
+int	str_separate_count(const char *s, char c)
 {
-	char	*ptr;
-	ptr = (char *)malloc(sizeof(char) * (i - start));
-	while (i < start)
+	int	i;
+	int	count;
+	int	flag;
+
+	i = 0;
+	count = 0;
+	flag = 0;
+	while (s[i])
 	{
-		ptr[i] = s[i];
-		printf("%c\n", s[i]);
+		if (s[i] != c && flag == 0)
+		{
+			flag = 1;
+			count++;
+		}
+		else if (s[i] == c)
+		{
+			flag = 0;
+		}
 		i++;
 	}
-	return (ptr);
+	return (count);
 }
 
+char	*str_cutter(const char	*s, int start, int end)
+{
+	char	*ptr;
+	int		i;
+
+	i = 0;
+	ptr = (char *)malloc(sizeof(char) * (end - start + 1));
+	while (start < end)
+	{
+		ptr[i] = s[start];
+		i++;
+		start++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
 char	**ft_split(const char *s, char c)
 {
 	char	**split;
-	char	*tmp;
-	int		i;
-	int		start;
+	int	i;
+	int j;
+	int	start;
 
-	split = (char **)malloc(sizeof(char *) * (ft_strlen(s) + 1));
-	if (!split)
-		return (NULL);
 	i = 0;
+	j = 0;
 	start = -1;
+	split = (char **)malloc(sizeof(char *) * (str_separate_count(s, c) + 1));
+	if (!split || !s)
+		return (NULL);
 	while (i <= (int)ft_strlen(s))
 	{
 		if (s[i] != c && start < 0)
 			start = i;
-		else if (s[i] == c)
+		else if ((s[i] == c || i == (int)ft_strlen(s)) && start >= 0)
 		{
-			tmp = (char *)malloc(sizeof(char) * i);
-			if(!tmp)
-				return (NULL);
-			tmp = ft_word_cut((char *)s, start, i);
+			split[j] = str_cutter(s, start, i);
+			start = -1;
+			j++;
 		}
 		i++;
 	}
-	split = &tmp;
+	split[j] = (char *)'\0';
 	return (split);
 }
 
 #include <stdio.h>
 int main(void)
 {
-    char *ptr = (char *)ft_split("AAbbBBbCC", 'b');
-    printf("%s\n", ptr);
+	char **ptr = ft_split("AAbbBBbCC", 'b');
+	for (int i = 0; i < 4; i++)
+	{
+		printf("[%d] == %s\n", i, (char *)ptr[i]);
+	}
 }
