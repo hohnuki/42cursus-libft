b7041a943b1283a17cb3e137339182ca5caec463
Hiroki Ohnuki <hohnuki@c1r17s7.42tokyo.jp>
2021-10-15 21:43:19 +0900

    ft_substr ft_strjoin comp

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hohnuki <hohnuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 20:28:50 by hohnuki           #+#    #+#             */
-/*   Updated: 2021/10/07 20:54:08 by hohnuki          ###   ########.fr       */
+/*   Updated: 2021/10/15 21:40:58 by hohnuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	char	*str;
 	int		i;
 
-	str = NULL;
 	i = 0;
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)));
-	if (!(str))
-		return (0);
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
 	while (*s1 != '\0')
 	{
 		str[i] = *s1++;
@@ -32,5 +33,6 @@ char	*ft_strjoin(const char *s1, const char *s2)
 		str[i] = *s2++;
 		i++;
 	}
+	str[i] = '\0';
 	return (str);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hohnuki <hohnuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 20:05:07 by hohnuki           #+#    #+#             */
-/*   Updated: 2021/10/07 20:27:29 by hohnuki          ###   ########.fr       */
+/*   Updated: 2021/10/15 21:24:33 by hohnuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,23 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	char	*str;
 	int		i;
 
-	str = NULL;
 	i = 0;
-	if (start >= ft_strlen(s))
-		return (str);
-	str = (char *)malloc(sizeof(char) * (len - (size_t)start));
-	if (!(str))
-		return (0);
-	while (len-- && len >= 0)
+	if (!s)
+		return (NULL);
+	if (start >= (unsigned int)ft_strlen(s) || len == 0)
 	{
-		str[i] = s[start - 1];
-		i++;
-		start++;
+		str = (char *)malloc(sizeof(char) * 1);
+		str[0] = '\0';
+		return (str);
 	}
+	str = (char *)malloc(sizeof(char) * ((int)len + 1));
+	if (!(str))
+		return (NULL);
+	while ((int)len >= 0 || str[i] != '\0')
+	{
+		str[i++ - 1] = s[(int)start++ - 1];
+		len--;
+	}
+	str[i - 1] = '\0';
 	return (str);
 }
