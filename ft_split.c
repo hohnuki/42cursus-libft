2ab2312ffe3e13a159e2f4da2949830c44e6d6d7
Hiroki Ohnuki <hohnuki@c1r26s1.42tokyo.jp>
2021-11-02 18:59:48 +0900

    split cut

--- ft_split.c
+++ ft_split.c
@@ -6,36 +6,11 @@
 /*   By: hohnuki <hohnuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 15:46:13 by hohnuki           #+#    #+#             */
-/*   Updated: 2021/10/22 17:22:16 by hohnuki          ###   ########.fr       */
+/*   Updated: 2021/11/02 18:59:18 by hohnuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	str_separate_count(const char *s, char c)
-{
-	int	i;
-	int	count;
-	int	flag;
-
-	i = 0;
-	count = 0;
-	flag = 0;
-	while (s[i])
-	{
-		if (s[i] != c && flag == 0)
-		{
-			flag = 1;
-			count++;
-		}
-		else if (s[i] == c)
-		{
-			flag = 0;
-		}
-		i++;
-	}
-	return (count);
-}
 
 static char	*str_cutter(const char	*s, int start, int end)
 {
@@ -54,20 +29,45 @@ static char	*str_cutter(const char	*s, int start, int end)
 	return (ptr);
 }
 
+static int	str_separate_count(const char *s, char c)
+{
+	int	i;
+	int	count;
+	int	flag;
+
+	i = 0;
+	count = 0;
+	flag = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			flag = 0;
+		else if (s[i] != c && flag == 0)
+		{
+			flag = 1;
+			count++;
+		}
+		i++;
+	}
+	return (count);
+}
+
 static void	split_loop(char **split, const char *s, char c)
 {
 	int	start;
 	int	i;
 	int	j;
+	int	len;
 
 	start = -1;
 	i = 0;
 	j = 0;
-	while (i <= (int)ft_strlen(s))
+	len = (int)ft_strlen(s);
+	while (i <= len)
 	{
 		if (s[i] != c && start < 0)
 			start = i;
-		else if ((s[i] == c || i == (int)ft_strlen(s)) && start >= 0)
+		else if ((s[i] == c || i == len) && start >= 0)
 		{
 			split[j] = str_cutter(s, start, i);
 			start = -1;
@@ -81,13 +81,7 @@ static void	split_loop(char **split, const char *s, char c)
 char	**ft_split(const char *s, char c)
 {
 	char	**split;
-	int		i;
-	int		j;
-	int		start;
 
-	i = 0;
-	j = 0;
-	start = -1;
 	if (!s)
 		return (NULL);
 	split = (char **)malloc(sizeof(char *) * (str_separate_count(s, c) + 1));
